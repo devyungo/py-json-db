cf83a00b3a6b21de22e58f8c508c713f80bc7f93
devyungo <128169293+devyungo@users.noreply.github.com>
2023-09-11 12:24:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ from dbpackage import dbmanage as dbm
 
 dbpath = "db.json"
 
+array= dbm.dbread(dbpath)
+
+def createstr():
+    global array
+    str = ""
+    for i in range(len(array)):
+        notasum = 0
+        for n in range(len(array[i]["notas"])):
+            notasum+=array[i]["notas"][n]/len(array[i]["notas"])
+        str+=f"{array[i]['nome']} - {notasum}\n"
+    return str
+
+print(createstr())
